84e9ac52c9b61e2cfebc4f3a9891728e05e42f74
tau3 <smr.oznob@gmail.com>
2021-10-28 11:47:57 +0400

    updates for laptop

---
+++ yoga_start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# touchpad
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=1
+
+# invert scroll
+synclient VertScrollDelta=-111
+synclient HorizScrollDelta=-111
+
+# change language
+setxkbmap -option 'grp:alt_shift_toggle'
+setxkbmap -layout us,ru
