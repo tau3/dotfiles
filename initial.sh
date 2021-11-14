317890c860f1d2a5db0fe9a45d5133122ec4c47b
tau3 <smr.oznob@gmail.com>
2021-11-15 03:36:19 +0400

    update config for home

--- initial.sh
+++ initial.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y curl ncdu htop vim mc ranger zsh git fzf silversearcher-ag tmux
+sudo apt-get install -y firmware-realtek firmware-amd-graphics
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tau3/dotfiles.git ~/git/dotfiles
